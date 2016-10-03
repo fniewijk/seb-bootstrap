0036ed7a7b8d4dc0492e77f9fe8edfe0be4ff1af
Folkert Niewijk <folkert.niewijk@marks-and-spencer.com>
2016-10-03 12:12:35 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # seb-bootstrap
+
+run npm install
+
+then
+
+node server.js
+
+# TODO
+
+call a Weather API with a Promise and display the London weather of today in a template.
---
+++ server.js
@@ -0,0 +1,38 @@
+//load the web rendering engine into node.js
+var express = require('express');
+//create a new app
+var app = express();
+//connect express to the handlebars rendering engine.
+var exphbs = require('express-handlebars');
+
+//set handlebars to be used as a rendering engine
+app.engine('hbs', exphbs({
+    extname:'hbs',
+    defaultLayout:'main.hbs'
+}));
+app.set('view engine', 'hbs');
+
+//whenever someone goes to the root, display this page.
+app.get('/', function (req, res) {
+    //render the index.hbs into the main.hbs.
+   res.render('index',{ 'title': 'I am rendered into the template'});
+});
+
+//TODO finish the weather app.
+app.get('/weather', function (req, res) {
+    //TODO do an AJAX request.
+    //Use the 'http' module.
+
+    //render the weather data in the weather.hbs template.
+   res.render('weather',{ 'data': {
+       'todo': 'data from the server'
+   }});
+});
+
+//start the server.
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+});
