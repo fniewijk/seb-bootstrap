7e517e15ba1f610ac45173f81e055d3227e52741
Sebastien Thibert <c2444980@MB683.local>
2016-10-12 11:22:54 +0100

    Updating CSS

--- assets/jquery.js
+++ assets/jquery.js
@@ -2,22 +2,22 @@ $(document).ready(function(){
 
   $(".col").mouseover(function(){
     $(this).css('background-color', '#598ad9');
-    $(this).css('border-radius', '5px');
+    $(this).css('border-radius', '3px');
   });
 
   $(".col").mouseout(function(){
     $(this).css('background-color', '#7fa5e2');
-    $(this).css('border-radius', '40px');
+    $(this).css('border-radius', '8px');
   });
 
   $("h1").mouseover(function(){
     $(this).css('background-color', '#598ad9');
-    $(this).css('border-radius', '5px');
+    $(this).css('border-radius', '3px');
   });
 
   $("h1").mouseout(function(){
     $(this).css('background-color', '#7fa5e2');
-    $(this).css('border-radius', '40px');
+    $(this).css('border-radius', '8px');
   });
 
   $("button").click(function(){
